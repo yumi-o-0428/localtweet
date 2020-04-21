fdf8b855d7327947b61777a26cc3edcbadfe53a6
yumi-o-0428 <ochabi0428@gmail.com>
2020-04-21 12:13:27 +0900

    association-user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200415114754) do
+ActiveRecord::Schema.define(version: 20200420101142) do
 
   create_table "taggings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "tag_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20200415114754) do
     t.text     "image",          limit: 65535
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
