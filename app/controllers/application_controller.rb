ba54493e2ce778391c413e35dee2c3ace273b605
yumi-o-0428 <ochabi0428@gmail.com>
2020-05-05 16:45:46 +0900

    search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,6 @@ class ApplicationController < ActionController::Base
   def set_search
     @search = Tweet.ransack(params[:q])
     @tweets = @search.result(distinct: true).includes(:tags)
-    if @search&.conditions&.first&.values&.first&.value&.present?
-      @search_word = @search.conditions.first.values.first.value
-    end
   end
 
 end
