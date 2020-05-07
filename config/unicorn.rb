7db1991b91f440bc5323bf498661022280d325a3
yumi-o-0428 <ochabi0428@gmail.com>
2020-05-07 09:24:53 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,7 @@ worker_processes 1
 
 working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 timeout 60
